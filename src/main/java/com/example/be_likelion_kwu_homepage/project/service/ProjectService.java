c7c536ad0a1d326f84327d738cc9804abc1e8e25
taesungs <70329288+taesungs@users.noreply.github.com>
2026-02-04 16:43:34 +0900

    feat: 프로젝트 아카이브 생성 기능 서비스 로직 추가

---
+++ src/main/java/com/example/be_likelion_kwu_homepage/project/service/ProjectService.java
@@ -0,0 +1,28 @@
+package com.example.be_likelion_kwu_homepage.project.service;
+
+import com.example.be_likelion_kwu_homepage.project.dto.request.CreateRequest;
+import com.example.be_likelion_kwu_homepage.project.entity.Project;
+import com.example.be_likelion_kwu_homepage.project.repository.ProjectRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProjectService {
+
+    private final ProjectRepository projectRepository;
+
+    @Transactional // 트랜잭션 : 예외 발생 -> 전체 롤백, 정상 종료 -> 커밋
+    public void create(CreateRequest req){
+        Project project = new Project(
+                req.title(),
+                req.subTitle(),
+                req.content(),
+                req.imageUrl(),
+                req.category(),
+                req.generation());
+        projectRepository.save(project);
+    }
+
+}
