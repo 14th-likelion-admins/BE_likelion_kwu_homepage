299fddf10dffbe5a6d084fcf03ac15b9d7e11918
taesungs <70329288+taesungs@users.noreply.github.com>
2026-02-05 15:42:58 +0900

    fix: 프로젝트 아카이브 생성 응답 dto 추가

--- src/main/java/com/example/be_likelion_kwu_homepage/project/service/ProjectService.java
+++ src/main/java/com/example/be_likelion_kwu_homepage/project/service/ProjectService.java
@@ -1,6 +1,7 @@
 package com.example.be_likelion_kwu_homepage.project.service;
 
 import com.example.be_likelion_kwu_homepage.project.dto.request.CreateRequest;
+import com.example.be_likelion_kwu_homepage.project.dto.response.CreateResponse;
 import com.example.be_likelion_kwu_homepage.project.entity.Project;
 import com.example.be_likelion_kwu_homepage.project.repository.ProjectRepository;
 import jakarta.transaction.Transactional;
@@ -14,15 +15,27 @@ public class ProjectService {
     private final ProjectRepository projectRepository;
 
     @Transactional // 트랜잭션 : 예외 발생 -> 전체 롤백, 정상 종료 -> 커밋
-    public void create(CreateRequest req){
-        Project project = new Project(
+    public CreateResponse create(CreateRequest req){
+        Project project = Project.create(
                 req.title(),
                 req.subTitle(),
                 req.content(),
                 req.imageUrl(),
                 req.category(),
-                req.generation());
-        projectRepository.save(project);
+                req.generation()
+        );
+
+       Project saved = projectRepository.save(project);
+
+        return new CreateResponse(
+                saved.getProjectid(),
+                saved.getTitle(),
+                saved.getSubTitle(),
+                saved.getContent(),
+                saved.getImageUrl(),
+                saved.getCategory().name(),
+                saved.getGeneration()
+        );
     }
 
 }
