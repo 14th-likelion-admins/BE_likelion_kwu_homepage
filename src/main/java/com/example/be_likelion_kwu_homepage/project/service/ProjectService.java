0c2fc46c459d17efe13b58e21086771b89710738
taesungs <70329288+taesungs@users.noreply.github.com>
2026-02-05 17:13:05 +0900

    feat: 프로젝트 생성 관련 서비스 로직 부분 응답값 수정

--- src/main/java/com/example/be_likelion_kwu_homepage/project/service/ProjectService.java
+++ src/main/java/com/example/be_likelion_kwu_homepage/project/service/ProjectService.java
@@ -1,7 +1,6 @@
 package com.example.be_likelion_kwu_homepage.project.service;
 
 import com.example.be_likelion_kwu_homepage.project.dto.request.CreateRequest;
-import com.example.be_likelion_kwu_homepage.project.dto.response.CreateResponse;
 import com.example.be_likelion_kwu_homepage.project.entity.Project;
 import com.example.be_likelion_kwu_homepage.project.repository.ProjectRepository;
 import jakarta.transaction.Transactional;
@@ -15,7 +14,7 @@ public class ProjectService {
     private final ProjectRepository projectRepository;
 
     @Transactional // 트랜잭션 : 예외 발생 -> 전체 롤백, 정상 종료 -> 커밋
-    public CreateResponse create(CreateRequest req){
+    public Long create(CreateRequest req){
         Project project = Project.create(
                 req.title(),
                 req.subTitle(),
@@ -26,16 +25,7 @@ public class ProjectService {
         );
 
        Project saved = projectRepository.save(project);
-
-        return new CreateResponse(
-                saved.getProjectid(),
-                saved.getTitle(),
-                saved.getSubTitle(),
-                saved.getContent(),
-                saved.getImageUrl(),
-                saved.getCategory().name(),
-                saved.getGeneration()
-        );
+       return saved.getId();
     }
 
 }
