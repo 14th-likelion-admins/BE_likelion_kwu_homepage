0f13169dbda48257b2061baa14f0e53667dd8d53
taesungs <70329288+taesungs@users.noreply.github.com>
2026-02-05 17:12:09 +0900

    feat: 프로젝트 ID명 변경 및 프로젝트 분류 클래스 분리

--- src/main/java/com/example/be_likelion_kwu_homepage/project/entity/Project.java
+++ src/main/java/com/example/be_likelion_kwu_homepage/project/entity/Project.java
@@ -1,5 +1,6 @@
 package com.example.be_likelion_kwu_homepage.project.entity;
 
+import com.example.be_likelion_kwu_homepage.domain.ProjectCategory;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -13,7 +14,7 @@ public class Project {
 
     @Id // 기본키
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long projectid; // 프로젝트 ID
+    private Long id; // 프로젝트 ID
 
     @Column(nullable = false, length = 100) // null 불가, 길이 100자 제한
     private String title; // 프로젝트 제목
@@ -21,7 +22,7 @@ public class Project {
     @Column(nullable = false, length = 100)
     private String subTitle; // 프로젝트 부제목
 
-    @Column(nullable = false, columnDefinition = "TEXT") // 텍스트 형식으로 컬럼 정의
+    @Column(nullable = false, columnDefinition = "TEXT", length = 500) // 텍스트 형식으로 컬럼 정의
     private String content; // 프로젝트 내용
 
     @Column(length = 500)
@@ -49,8 +50,4 @@ public class Project {
         return new Project(title, subTitle, content, imageUrl, category, generation);
     }
 
-    // 프로젝트 분류 Enum 클래스로 별도 관리
-    public enum ProjectCategory {
-        WEB, APP
-    }
 }
