abe754b80ce80ece5945a5123f22d6438d356f64
taesungs <70329288+taesungs@users.noreply.github.com>
2026-02-05 17:11:32 +0900

    feat: 프로젝트 분류 클래스 분리에 따른 경로 수정

--- src/main/java/com/example/be_likelion_kwu_homepage/project/dto/request/CreateRequest.java
+++ src/main/java/com/example/be_likelion_kwu_homepage/project/dto/request/CreateRequest.java
@@ -1,7 +1,7 @@
 package com.example.be_likelion_kwu_homepage.project.dto.request;
 
 
-import com.example.be_likelion_kwu_homepage.project.entity.Project;
+import com.example.be_likelion_kwu_homepage.domain.ProjectCategory;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -20,7 +20,7 @@ public record CreateRequest(
         String imageUrl,
 
         @NotNull
-        Project.ProjectCategory category,
+        ProjectCategory category,
 
         @NotNull
         Integer generation
