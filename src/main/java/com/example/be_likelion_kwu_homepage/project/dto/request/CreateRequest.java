52aac53256c7cd67c3e1426d0fb9e9e7fa022684
taesungs <70329288+taesungs@users.noreply.github.com>
2026-02-05 15:41:38 +0900

    fix: 프로젝트 분류 및 기수 상태 변경

--- src/main/java/com/example/be_likelion_kwu_homepage/project/dto/request/CreateRequest.java
+++ src/main/java/com/example/be_likelion_kwu_homepage/project/dto/request/CreateRequest.java
@@ -1,15 +1,27 @@
 package com.example.be_likelion_kwu_homepage.project.dto.request;
 
 
+import com.example.be_likelion_kwu_homepage.project.entity.Project;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record CreateRequest(
-        @NotBlank String title,
-        @NotBlank String subTitle,
-        @NotBlank String content,
-        @Size(max = 500) String imageUrl,
-        String category,
-        @NotNull Long generation
+        @NotBlank
+        String title,
+
+        @NotBlank
+        String subTitle,
+
+        @NotBlank
+        String content,
+
+        @Size(max = 500)
+        String imageUrl,
+
+        @NotNull
+        Project.ProjectCategory category,
+
+        @NotNull
+        Integer generation
 ){}
