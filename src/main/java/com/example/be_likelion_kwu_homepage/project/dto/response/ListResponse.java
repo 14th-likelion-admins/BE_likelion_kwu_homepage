1605e0ef5bf38a6aec88f583a7019f79583c7b76
taesungs <70329288+taesungs@users.noreply.github.com>
2026-02-06 23:17:46 +0900

    feat: 프로젝트 전체조회 응답 dto 추가

---
+++ src/main/java/com/example/be_likelion_kwu_homepage/project/dto/response/ListResponse.java
@@ -0,0 +1,8 @@
+package com.example.be_likelion_kwu_homepage.project.dto.response;
+
+public record ListResponse(
+        String title,
+        String subTitle,
+        String imageUrl,
+        Long id
+) {}
