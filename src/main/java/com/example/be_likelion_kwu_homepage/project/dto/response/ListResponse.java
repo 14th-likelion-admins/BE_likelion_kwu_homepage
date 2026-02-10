7a29ddebded037f12bc955973c8bbe76d42294f6
taesungs <70329288+taesungs@users.noreply.github.com>
2026-02-10 18:08:26 +0900

    feat: 전체조회 응답 DTO에 정적 팩토리 메서드 추가

--- src/main/java/com/example/be_likelion_kwu_homepage/project/dto/response/ListResponse.java
+++ src/main/java/com/example/be_likelion_kwu_homepage/project/dto/response/ListResponse.java
@@ -1,8 +1,19 @@
 package com.example.be_likelion_kwu_homepage.project.dto.response;
 
+import com.example.be_likelion_kwu_homepage.project.entity.Project;
+
 public record ListResponse(
         String title,
         String subTitle,
         String imageUrl,
-        Long id
-) {}
+        Long projectId
+) {
+    public static ListResponse from(Project project) {
+        return new ListResponse(
+                project.getTitle(),
+                project.getSubTitle(),
+                project.getImageUrl(),
+                project.getId()
+        );
+    }
+}
