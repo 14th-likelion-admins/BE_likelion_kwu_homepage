d0586685381899b90183350cf4ebafb28720d51e
taesungs <70329288+taesungs@users.noreply.github.com>
2026-02-12 16:36:01 +0900

    feat: 전체조회, 상세조회 응답 통일을 위해 id 위치 수정

--- src/main/java/com/example/be_likelion_kwu_homepage/project/dto/response/ListResponse.java
+++ src/main/java/com/example/be_likelion_kwu_homepage/project/dto/response/ListResponse.java
@@ -3,17 +3,17 @@ package com.example.be_likelion_kwu_homepage.project.dto.response;
 import com.example.be_likelion_kwu_homepage.project.entity.Project;
 
 public record ListResponse(
+        Long projectId,
         String title,
         String subTitle,
-        String imageUrl,
-        Long projectId
+        String imageUrl
 ) {
     public static ListResponse from(Project project) {
         return new ListResponse(
+                project.getId(),
                 project.getTitle(),
                 project.getSubTitle(),
-                project.getImageUrl(),
-                project.getId()
+                project.getImageUrl()
         );
     }
 }
