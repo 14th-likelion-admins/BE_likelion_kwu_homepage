7650f1fa2266aecaa4f7710c102269074f1b47a7
taesungs <70329288+taesungs@users.noreply.github.com>
2026-02-22 20:58:04 +0900

    rename: 수정 요청 DTO명 변경

--- src/main/java/com/example/be_likelion_kwu_homepage/project/dto/request/UpdateProjectRequest.java
+++ src/main/java/com/example/be_likelion_kwu_homepage/project/dto/request/UpdateProjectRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public record UpdateRequest(
+public record UpdateProjectRequest(
         @NotBlank
         String title,
 
