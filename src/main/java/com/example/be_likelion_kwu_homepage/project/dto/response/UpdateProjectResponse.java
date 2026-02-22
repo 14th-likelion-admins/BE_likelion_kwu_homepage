70bab0ad1c41e99847268a22a15b32575b8eca36
taesungs <70329288+taesungs@users.noreply.github.com>
2026-02-22 20:58:12 +0900

    rename: 수정 응답 DTO명 변경

--- src/main/java/com/example/be_likelion_kwu_homepage/project/dto/response/UpdateProjectResponse.java
+++ src/main/java/com/example/be_likelion_kwu_homepage/project/dto/response/UpdateProjectResponse.java
@@ -1,24 +1,25 @@
 package com.example.be_likelion_kwu_homepage.project.dto.response;
 
+import com.example.be_likelion_kwu_homepage.project.domain.ProjectCategory;
 import com.example.be_likelion_kwu_homepage.project.entity.Project;
 
-public record UpdateResponse(
+public record UpdateProjectResponse(
         Long projectId,
         String title,
         String subTitle,
         String content,
         String imageUrl,
-        String category,
+        ProjectCategory category,
         Integer generation
 ) {
-    public static UpdateResponse from(Project project) {
-        return new UpdateResponse(
+    public static UpdateProjectResponse from(Project project) {
+        return new UpdateProjectResponse(
                 project.getId(),
                 project.getTitle(),
                 project.getSubTitle(),
                 project.getContent(),
                 project.getImageUrl(),
-                project.getCategory().name(),
+                project.getCategory(),
                 project.getGeneration()
         );
     }
